7acabd2a198f25c015ec518b54118ee9eaaa9dee
NahdaaJ <nahdaajawed@gmail.com>
2025-05-23 16:29:09 +0100

    Deleting and restarting: wanted to use different software and better practices

---
+++ CosyShelf/CosyShelf.UI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CosyShelf.UI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CosyShelf/CosyShelf.UI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CosyShelf.UI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
